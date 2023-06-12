4b393541f494f6988ed7b17f9cf93d1ea0b2facc
EDINSON <cuentapersonal145@gmail.com>
2023-06-12 15:44:18 -0500

    add env

---
+++ env.bat
@@ -0,0 +1,5 @@
+@echo off
+cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Tecnologias\Django\env\Scripts
+call activate.bat
+cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Trabajo de grado\Pagina web\BackEnd Django\Gestion_Proyectos_FIT
+cmd /k
