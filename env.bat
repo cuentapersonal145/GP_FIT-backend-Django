91bfd0857fa8d997fb5c1b4e68b7b1bb44c7e210
EDINSON <cuentapersonal145@gmail.com>
2023-06-12 15:47:00 -0500

    .gitignore is now working01~

--- env.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Tecnologias\Django\env\Scripts
-call activate.bat
-cd D:\MIS DOCUMENTOS\LENOVO\Desktop\Trabajo de grado\Pagina web\BackEnd Django\Gestion_Proyectos_FIT
-cmd /k
